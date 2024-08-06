f23b8d9a9e621944a94737ada04b14feec2c450d
Treasure <odetokuntreasure6@gmail.com>
2024-08-06 23:38:57 +0100

    minor-details

--- src/UI/ProjectList.jsx
+++ src/UI/ProjectList.jsx
@@ -8,7 +8,7 @@ function ProjectList({ el }) {
     <div className="w-[20rem] bg-[#FFFFFF14] p-5 rounded-lg">
       <div className="">
         <h3 className="text-[#fff] font-bold text-[1rem] mb-5">{name}</h3>
-        <img src={`${image?.image} `} alt={name} className="" />
+        <img src={`${image?.image} `} alt={name} className="mb-2" />
         <p className="text-[#8c8c8c] mb-5">{detail}</p>
         <button className="flex justify-center items-center gap-2">
           <a className="text-[#fff] text-[1rem]" href={link}>
